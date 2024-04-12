05b049387a6a6c4c0a47848dcc505483c3665c8c
Antônio Sales <antoniosales1003@gmail.com>
2024-04-12 17:14:48 -0300

    tests: add in-memory-users-repository

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,30 @@
+import { Prisma, User } from '@prisma/client'
+import { UsersRepository } from '../users-repository'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public users: User[] = []
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: data.id ? data.id : 'user-1',
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    }
+
+    this.users.push(user)
+
+    return user
+  }
+
+  async findByEmail(email: string) {
+    const user = this.users.find(user => user.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+}
