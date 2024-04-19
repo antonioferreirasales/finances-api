d6380a17913b3301c5d677a23f123cf07b063285
Antônio Sales <antoniosales1003@gmail.com>
2024-04-19 09:42:59 -0300

    tests: add user controllers tests

--- src/http/controllers/users/register.spec.ts
+++ src/http/controllers/users/register.spec.ts
@@ -1,3 +1,23 @@
-import { it } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
 
-it('ok', () => 'ok')
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
